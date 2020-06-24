6853d59b132e2c8c05643bc9686b1cac22e318e8
tdyer21 <tdyer@ucsc.edu>
2020-06-23 17:16:23 -0700

    Stylesheet workaround and added images to test

--- src/App.js
+++ src/App.js
@@ -5,8 +5,24 @@ import './index.css';
 class Product extends React.Component {
     render() {
         return (
-            <div>
-                something
+            <div className='item'>
+                <div className='image'>
+                    <img src={require('./images/slug.png')}
+                         alt=' ' />
+                </div>
+                <div className='middle aligned content'>
+                    <div className='description'>
+                        <a>Sammy The Slug</a>
+                        <p>University of California, Santa Cruz</p>
+                    </div>
+                    <div className='extra'>
+                        <span>Submitted by:</span>
+                        <img className='ui avatar image'
+                             src={require('./images/prof.jpg')}
+                             alt=' '
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
@@ -16,12 +32,11 @@ class ProductList extends React.Component {
     render() {
         return (
             <div className='ui stackable items'>
-                Hello, I am a basic React component
+                <Product />
             </div>
         );
     }
 }
-console.log('what');
 
 ReactDOM.render(
     <ProductList />,
